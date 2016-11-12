2edf37c0101550f705f7ceceabb242695f730b5f
clintmann <clintmann3@gmail.com>
2016-11-12 12:55:22 -0500

    added bash script

--- chive_agent_aci.py
+++ chive_agent_aci.py
@@ -35,52 +35,55 @@ cookies = {}
 cookies['APIC-Cookie'] = auth_token
 
 # ---- make REST API Call section  -------
+try:
+    while True:
+        # Generate URL used for REST API call to get 5minute temperature data
+        node_url = base_url + 'class/eqptTemp5min.json'
 
-# Generate URL used for REST API call to get 5minute temperature data
-node_url = base_url + 'class/eqptTemp5min.json'
+        get_nodes = requests.get(node_url, cookies=cookies, verify=False).json()
 
-get_nodes = requests.get(node_url, cookies=cookies, verify=False).json()
+        rootNodeList = get_nodes["imdata"]
 
-rootNodeList = get_nodes["imdata"]
+        # ---- parse data collected from REST API call section  -------
+        # Create variables to be used to sort data
+        dn_cnt = 0  # counter for dn attribute in rootNodeList
+        device_lst = []  # list for device names
+        curr_temp_lst = []  # list for tempature readings
+        day_time_lst = []  # list for day/time of readings
 
-# ---- parse data collected from REST API call section  -------
-# Create variables to be used to sort data
-dn_cnt = 0  # counter for dn attribute in rootNodeList
-device_lst = []  # list for device names
-curr_temp_lst = []  # list for tempature readings
-day_time_lst = []  # list for day/time of readings
+        for device in rootNodeList:
+            dn = device['eqptTemp5min']['attributes']['dn']  # find dn field
+            curr_temp = device['eqptTemp5min']['attributes']['currentLast']  # current temp field
+            day_time = device['eqptTemp5min']['attributes']['repIntvEnd']   # day/time field
 
-for device in rootNodeList:
-    dn = device['eqptTemp5min']['attributes']['dn']  # find dn field
-    curr_temp = device['eqptTemp5min']['attributes']['currentLast']  # current temp field
-    day_time = device['eqptTemp5min']['attributes']['repIntvEnd']   # day/time field
+            # change to ascii - type is now string
+            dn_ascii = dn.encode("utf8")
+            # change to ascii - type is now string
+            curr_temp_ascii = curr_temp.encode("utf8")
+            # change to ascii - type is now string
+            day_time_ascii = day_time.encode("utf8")
 
-    # change to ascii - type is now string
-    dn_ascii = dn.encode("utf8")
-    # change to ascii - type is now string
-    curr_temp_ascii = curr_temp.encode("utf8")
-    # change to ascii - type is now string
-    day_time_ascii = day_time.encode("utf8")
+            sensor = re.findall(r'sup/sensor-(.*?)/', dn_ascii)
 
-    sensor = re.findall(r'sup/sensor-(.*?)/', dn_ascii)
+            if '3' in sensor:  # Interested in only sensor-3
+                # this will find ['node-x'] ex . topology/pod-1/node-101/sys/ch/supslot-1/sup/sensor-3/CDeqptTemp5min
+                devname = re.findall(r'.*/.*/(.*?)/.*/.*/.*/.*./.*/.*', dn_ascii)
+                dn_cnt += 1  # Add 1 to dn_cnt
 
-    if '3' in sensor:  # Interested in only sensor-3
-        # this will find ['node-x'] ex . topology/pod-1/node-101/sys/ch/supslot-1/sup/sensor-3/CDeqptTemp5min
-        devname = re.findall(r'.*/.*/(.*?)/.*/.*/.*/.*./.*/.*', dn_ascii)
-        dn_cnt += 1  # Add 1 to dn_cnt
+                device_lst.append(devname)
+                curr_temp_lst.append(curr_temp_ascii)
+                day_time_lst.append(day_time_ascii)
 
-        device_lst.append(devname)
-        curr_temp_lst.append(curr_temp_ascii)
-        day_time_lst.append(day_time_ascii)
-
-
-# Display to standard output for testing - visualize data exists
-i = 0
-print "----                 TABLE                   ----"
-print " Node                Temp               Day/Time "
-while i < dn_cnt:
-    print device_lst[i], '       ', curr_temp_lst[i], '     ', day_time_lst[i]
-    i += 1  # Add 1 to variable i
+        # Display to standard output for testing - visualize data exists
+        i = 0
+        print "----                 TABLE                   ----"
+        print " Node                Temp               Day/Time "
+        while i < dn_cnt:
+            print device_lst[i], '       ', curr_temp_lst[i], '     ', day_time_lst[i]
+            i += 1  # Add 1 to variable i
+        time.sleep(60)
+except KeyboardInterrupt:
+    print('Manual break by user - CTRL-C')
 
 # ---- mySQL section  -------
 
