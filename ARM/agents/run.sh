3986a013d233d3adc234b86e4370436295572631
3pings <jbarksdale@threepings.com>
2016-11-21 21:37:14 -0500

    Changed:
        - Paths in Docker file

---
+++ ARM/agents/run.sh
@@ -0,0 +1,13 @@
+echo "What is your APIC IP Address: "
+read apic_ip
+echo "What is your APIC Username: "
+read apic_username
+echo "What is you APIC Password: "
+read -s apic_password
+echo
+
+if
+docker run -ite APIC_IP=$apic_ip \
+    -e APIC_USERNAME=$apic_username \
+    -e APIC_PASSWORD=$apic_password \
+    3pings/chive_agent:latest
