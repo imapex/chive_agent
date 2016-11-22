c97ca79aa17aed7b9d1bba4aa22bd765e9408da6
3pings <jbarksdale@threepings.com>
2016-11-22 09:34:28 -0500

    Changed:
        - Paths in Docker file

--- arm/agents/chive_agent_aci.py
+++ arm/agents/chive_agent_aci.py
@@ -12,6 +12,8 @@ import time
 apic_ip = os.environ['APIC_IP']
 apic_username = os.environ['APIC_USERNAME']
 apic_password = os.environ['APIC_PASSWORD']
+chive_app = os.environ['CHIVE_APP']
+
 
 def connect_apic(apic_ip):
     # -- connect to APIC section  --
@@ -153,7 +155,7 @@ def send2_RESTAPI(obj):
     try:
         while True:
             headers = {"Content-Type": "application/json"}
-            rsp = requests.post('http://127.0.0.1:5000/device', headers=headers, data=json.dumps(obj))
+            rsp = requests.post('http://' + chive_app + '/device', headers=headers, data=json.dumps(obj))
             return rsp.ok
             # print(requests.post('http://127.0.0.1:5000/device', headers=headers, json=data))
     except:
